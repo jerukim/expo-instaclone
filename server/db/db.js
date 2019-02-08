1fc4154cb7a20a42346924c724e9a7981b610518
jeru <mikurej95@gmail.com>
2019-02-08 07:42:43 -0600

    fixed development environment secrets requirement

--- server/db/db.js
+++ server/db/db.js
@@ -1,22 +1,22 @@
 const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 
-const dev = process.env.NODE_ENV === 'development'
+const dev = process.env.NODE_ENV === 'development';
 
-const db = dev ? new Sequelize(
-  `postgres://localhost:5432/${pkg.name}`,
-  {
-    logging: false
-  }
-) : new Sequelize(
-  process.env.RDS_DB_NAME,
-  process.env.RDS_USERNAME,
-  process.env.RDS_PASSWORD,
-  {
-    host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
-    dialect: 'postgres',
-  }
-);
+const db = dev
+  ? new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.RDS_DB_NAME,
+      process.env.RDS_USERNAME,
+      process.env.RDS_PASSWORD,
+      {
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        dialect: 'postgres',
+        logging: false,
+      }
+    );
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') require('../secrets');
+console.log(process.env.NODE_ENV);
+
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
