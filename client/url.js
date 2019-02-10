23b7c35228fa90de4be6c76bdc072aac208d8852
jeru <mikurej95@gmail.com>
2019-02-10 02:40:24 -0600

    updated base url to work with simulator and device

--- client/url.js
+++ client/url.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// production
 // export const baseURL = 'Instaclone-env-1.cnnhnf83mp.us-east-2.elasticbeanstalk.com';
-export const baseURL = 'http://localhost:8080';
+
+// development
+export const baseURL = 'http://192.168.1.68:8080';
 
 const ax = axios.create({
   baseURL,
